876abf39fc500a96b959d76cb8bb86eaacb6e35d
Aditya kumar <adityakumarwww333@gmail.com>
2025-01-09 00:29:14 +0530

    Create Tempreature converter .java

---
+++ Tempreature converter .java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class CtoF {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double cel,fah;
+        
+        //Getting input Celsius Tempreture by user
+        System.out.print("\nEnter Tempreture in Celsius: ");
+        cel=scan.nextDouble();
+        
+        //Converting Celsius to fahrenheit
+        fah=1.8*cel+32;
+        
+        //Showing Fahrenheit Tempreture to user
+        System.out.println("\nFahrenheit : "+fah);
+
+    }
+}
